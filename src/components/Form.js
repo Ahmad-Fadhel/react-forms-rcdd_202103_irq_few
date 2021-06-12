91491b66abf637905ee641532e01a43bf1308b44
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-06-12 01:31:38 +0000

    Automatically backed up by Learn

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,37 +1,6 @@
 import React from 'react';
 
 class Form extends React.Component {
-  state = {
-    firstName: "John",
-    lastName: "Henry",
-    submittedData: []
-  }
-
-  handleFirstNameChange = event => {
-    this.setState({
-      firstName: event.target.value
-    })
-  }
-
-  handleLastNameChange = event => {
-    this.setState({
-      lastName: event.target.value
-    })
-  }
-  
-   handleSubmit = event => {
-    event.preventDefault();
-    let formData = {
-      firstName: this.state.firstName,
-      lastName: this.state.lastName
-    }
-
-    let dataArray = this.state.submittedData.concat(formData);
-    this.setState({
-      submittedData: dataArray
-    })
-    }
-  }
 
   render() {
     return (
